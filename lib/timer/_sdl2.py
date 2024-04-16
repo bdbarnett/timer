dd760066eb04ad6acab4e5f4a079b05ebcb64912
Brad Barnett <127794626+bdbarnett@users.noreply.github.com>
2024-04-16 11:28:37 -0500

    Update _sdl2.py

--- lib/timer/_sdl2.py
+++ lib/timer/_sdl2.py
@@ -1,20 +1,22 @@
+"""
+This script is NOT working.  See the comments below.
+"""
 from ._timerbase import _TimerBase
 import ffi
-from micropython import const
 
-SDL_INIT_TIMER = const(0x00000001)
+SDL_INIT_TIMER = 1
 
 _sdl = ffi.open("libSDL2-2.0.so.0")
 
 SDL_Init = _sdl.func("i", "SDL_Init", "I")
-SDL_AddTimer = _sdl.func("p", "SDL_AddTimer", "iCp")
+SDL_AddTimer = _sdl.func("p", "SDL_AddTimer", "iCp")  # Not sure if this line is correct
 SDL_RemoveTimer = _sdl.func("v", "SDL_RemoveTimer", "p")
 
 class Timer(_TimerBase):
     def _start(self):
         SDL_Init(SDL_INIT_TIMER)
-        cb = ffi.callback("v", self._callback, "i", lock=True)
-        self._timer = SDL_AddTimer(int(self._interval), cb.cfun(), self.id)
+        cb = ffi.callback("v", self._timer_callback, "i", lock=True)  # Not sure if this line is correct
+        self._timer = SDL_AddTimer(int(self._interval), cb.cfun(), self.id)  # Not sure if this line is correct
 
     def _stop(self):
         if self._timer:
