2600bf8d0b408e4399bc529b816f1868d11aea8d
Brad Barnett <bbarnett@gmail.com>
2024-05-15 20:08:57 -0500

    add ONE_SHOT test

--- examples/timer_simpletest.py
+++ examples/timer_simpletest.py
@@ -8,6 +8,15 @@ from timer import Timer
 def testfunc(t):
     print(".")
 
-tim = Timer()
-tim.init(mode=Timer.PERIODIC, period=500, callback=testfunc)
-# timer.deinit()
+def cancel(t):
+    print("Cancelling timer 1.")
+    tim1.deinit()
+
+# Create a timer that prints a dot every 500ms
+tim1 = Timer()
+print("Starting timer 1.")
+tim1.init(mode=Timer.PERIODIC, period=500, callback=testfunc)
+
+# Create a timer that cancels the first timer after 5 seconds
+tim2 = Timer()
+tim2.init(mode=Timer.ONE_SHOT, period=5000, callback=cancel)
