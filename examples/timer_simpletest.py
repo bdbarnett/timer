0a1260e838b20f91e49aab2a4b360f4b85f6e195
Brad Barnett <bbarnett@gmail.com>
2024-05-18 22:09:46 -0500

    use schedule

--- examples/timer_simpletest.py
+++ examples/timer_simpletest.py
@@ -3,20 +3,30 @@ This is a simple test script that tests the basic functionality of the timer cla
 """
 
 from timer import Timer
+try:
+    from micropython import schedule
+except ImportError:
+    schedule = lambda f, x: f(x)
 
 
-def testfunc(t):
+def testfunc(_):
     print(".")
 
-def cancel(t):
+def schedule_testfunc(t):
+    schedule(testfunc, 0)
+
+def cancel(_):
     print("Cancelling timer 1.")
     tim1.deinit()
+
+def schedule_cancel(t):
+    schedule(cancel, 0)
 
 # Create a timer that prints a dot every 500ms
 tim1 = Timer()
 print("Starting timer 1.")
-tim1.init(mode=Timer.PERIODIC, period=500, callback=testfunc)
+tim1.init(mode=Timer.PERIODIC, period=500, callback=schedule_testfunc)
 
 # Create a timer that cancels the first timer after 5 seconds
 tim2 = Timer()
-tim2.init(mode=Timer.ONE_SHOT, period=5000, callback=cancel)
+tim2.init(mode=Timer.ONE_SHOT, period=5000, callback=schedule_cancel)
